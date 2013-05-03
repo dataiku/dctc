ffcb001a00903e0daf7b07453eb5d2ca80cb4898
Clement Stenac <clement.stenac@dataiku.com>
2013-05-03 18:50:22 +0200

    Fixes

--- packager/create-dctc-selfextracting.sh
+++ packager/create-dctc-selfextracting.sh
@@ -12,7 +12,7 @@ echo '#! /bin/sh' >> $DEST
 echo 'TMPFILE=`mktemp /tmp/temp.XXXX`' >> $DEST
 echo "PAYLOAD_START=6" >> $DEST
 echo 'tail -n +$PAYLOAD_START $0 > $TMPFILE' >> $DEST
-echo 'eval java -jar $TMPFILE $*' >> $DEST
+echo 'java -jar $TMPFILE "$@"' >> $DEST
 echo 'RET=$?; rm -f $TMPFILE; exit $RET' >> $DEST
 cat $JARFILE >> $DEST
 
