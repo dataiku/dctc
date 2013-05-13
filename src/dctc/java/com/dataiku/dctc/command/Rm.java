e5183f396f1fd9a7417a6feb69263c29dd82659e
David Moreira <david.moreira@dataiku.com>
2013-05-13 11:51:12 +0200

    rm: Delete even if not verbose.
    Closes #37.
    
    * src/dctc/java/com/dataiku/dctc/command/Rm.java: Here.

--- src/dctc/java/com/dataiku/dctc/command/Rm.java
+++ src/dctc/java/com/dataiku/dctc/command/Rm.java
@@ -143,6 +143,8 @@ public class Rm extends Command {
                             verbose(son.givenName());
                             del(son);
                         }
+                    } else {
+                        del(arg);
                     }
                 }
             }
