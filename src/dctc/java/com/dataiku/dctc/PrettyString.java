37ba352ce9b493299d5bb44b993d86e5d59af82e
David Moreira <david.moreira@dataiku.com>
2013-05-16 11:34:10 +0200

    Extends PrettyString.
    * src/dctc/java/com/dataiku/dctc/PrettyString.java: s{imple}quoted,
      p{retty}quoted, eol, cat, s{pace}cat, n{ew}l{ine}cat.

--- src/dctc/java/com/dataiku/dctc/PrettyString.java
+++ src/dctc/java/com/dataiku/dctc/PrettyString.java
@@ -4,13 +4,40 @@ public class PrettyString {
     public static String quoted(String msg) {
         return "\"" + msg + "\"";
     }
+    public static String squoted(String msg) {
+        return "'" + msg + "'";
+    }
+    public static String pquoted(String msg) {
+        return "`" + msg + "'";
+    }
     public static String nl(String... msgs) {
-        String eol = System.getProperty("line.separator");
         String res = "";
         for(String msg: msgs) {
-            res += msg + eol;
+            res += msg + eol();
         }
 
         return res;
     }
+    public static String eol() {
+        return System.getProperty("line.separator");
+    }
+    public static String cat(String concatenateStr, String... msg) {
+        StringBuilder b = new StringBuilder();
+        if (msg.length > 0) {
+            b.append(msg[0]);
+            for(int i = 1; i < msg.length; ++i) {
+                b.append(concatenateStr);
+                b.append(msg[i]);
+            }
+        }
+        return b.toString();
+    }
+    public static String scat(String... msg) {
+        return cat(" ", msg);
+    }
+    // Do not put a trailing eol.
+    public static String nlcat(String... msg) {
+        return cat(eol(), msg);
+    }
+
 }
