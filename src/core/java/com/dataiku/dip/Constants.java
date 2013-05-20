78c887f6df83d0766070e08114cfa97d4f674aee
Clement Stenac <clement.stenac@dataiku.com>
2013-05-20 14:54:44 +0200

    Remove useless stuff

--- src/core/java/com/dataiku/dip/Constants.java
+++ src/core/java/com/dataiku/dip/Constants.java
@@ -1,7 +1,4 @@
 package com.dataiku.dip;
 
 public class Constants {
-    public static final String ELASTICSEARCH = "Elasticsearch";
-    public static final String SINGLE_FILE = "SingleFile";
-    public static final String FS = "Filesystem";
 }
