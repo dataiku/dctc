160961544ee66bfe2a0e0c38604d3f80c7441554
David Moreira <david.moreira@dataiku.com>
2013-05-22 10:01:02 +0200

    pretty string: Get Object instead of String as parameters.
    * src/dctc/java/com/dataiku/dctc/PrettyString.java: Here. Make cat
      protected (quite dangerous).

--- src/dctc/java/com/dataiku/dctc/PrettyString.java
+++ src/dctc/java/com/dataiku/dctc/PrettyString.java
@@ -21,10 +21,10 @@ public class PrettyString {
     public static String eol() {
         return System.getProperty("line.separator");
     }
-    public static String cat(String concatenateStr, String... msg) {
+    protected static String cat(String concatenateStr, Object... msg) {
         StringBuilder b = new StringBuilder();
         if (msg.length > 0) {
-            b.append(msg[0]);
+            b.append(msg[0].toString());
             for(int i = 1; i < msg.length; ++i) {
                 b.append(concatenateStr);
                 b.append(msg[i]);
@@ -32,11 +32,11 @@ public class PrettyString {
         }
         return b.toString();
     }
-    public static String scat(String... msg) {
+    public static String scat(Object... msg) {
         return cat(" ", msg);
     }
     // Do not put a trailing eol.
-    public static String nlcat(String... msg) {
+    public static String nlcat(Object... msg) {
         return cat(eol(), msg);
     }
 
