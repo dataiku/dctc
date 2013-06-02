d857963095813bd90345281014ed62d5e94d83a1
Clement Stenac <clement.stenac@dataiku.com>
2013-06-02 18:36:29 +0200

    I don't understand much but at least, this gives a "good" version string for head

--- dctc-hash-update.sh
+++ dctc-hash-update.sh
@@ -4,6 +4,6 @@ if git describe; then
     describe=`git describe`
 
 else
-    describe="0.0"
+    describe="0.2.1-`git log --pretty=format:'%h' -n1`"
 fi
     sed -ie "s/XXX_GIT_VERSION_XXX/$describe/" $1
