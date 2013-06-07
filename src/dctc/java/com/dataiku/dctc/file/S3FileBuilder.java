109c98b2d192d46fb851ab8a606b0b9656be2b55
David Moreira <david.moreira@dataiku.com>
2013-06-07 09:59:49 +0200

    style: Less space.
    * src/dctc/java/com/dataiku/dctc/file/S3FileBuilder.java: Here.

--- src/dctc/java/com/dataiku/dctc/file/S3FileBuilder.java
+++ src/dctc/java/com/dataiku/dctc/file/S3FileBuilder.java
@@ -20,7 +20,7 @@ public class S3FileBuilder extends ProtocolFileBuilder {
     @Override
     public boolean validateAccountParams(String account, Params p) {
         return checkAllowedOnly(account, p, new String[]{"access_key", "secret_key", "default_path"})
-            ||     checkMandatory(account, p, "access_key")
+            || checkMandatory(account, p, "access_key")
             ||checkMandatory(account, p, "secret_key");
     }
 
