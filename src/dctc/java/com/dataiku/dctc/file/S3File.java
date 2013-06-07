805ef4aac3dafc2fa77424bbf4df2a5ecf3650ec
David Moreira <david.moreira@dataiku.com>
2013-06-07 16:04:10 +0200

    s3: Missing trailing file separator added.
    * src/dctc/java/com/dataiku/dctc/file/S3File.java (resolve): Unless
      this one, no files are listed.

--- src/dctc/java/com/dataiku/dctc/file/S3File.java
+++ src/dctc/java/com/dataiku/dctc/file/S3File.java
@@ -426,7 +426,8 @@ public class S3File extends BucketBasedFile {
                 recursiveFileList = new ArrayList<S3ObjectSummary>();
                 try {
                     ListObjectsRequest listRequest = new ListObjectsRequest()
-                        .withBucketName(bucket).withPrefix(path.isEmpty() ? path : null);
+                        .withBucketName(bucket)
+                        .withPrefix(path.isEmpty() ? null : path + "/");
                     if (!autoRecur) {
                         listRequest.setDelimiter(fileSeparator());
                         dirs = new ArrayList<String>();
