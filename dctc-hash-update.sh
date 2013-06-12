192466dedc37ae863e424a45112456e28dd91960
David Moreira <david.moreira@dataiku.com>
2013-06-12 09:33:48 +0200

    version: Revert the file. More check. Be secure.
    * dctc-hash-update.sh: File at any error. Pretty print the
      version. Check if has an argument (no hag). Revert the file (even if
      assumed unchanged).

--- dctc-hash-update.sh
+++ dctc-hash-update.sh
@@ -1,9 +1,21 @@
 #! /bin/sh
-git describe --tags
+
+set -e
+
+echo The version is: `git describe --tags`
+
+if [ "x$1" = "x" ]; then
+    echo Need an input file.
+    exit 1
+fi
+
 if git describe --tags > /dev/null 2>&1; then
     describe="`git describe --tags`"
 
 else
     describe="0.0"
 fi
-sed -ie "s/XXX_GIT_VERSION_XXX/$describe/" $1
+
+git checkout -- "$1" || echo -- $1 is not gitted # Reset the file.
+
+sed -ie "s/XXX_GIT_VERSION_XXX/$describe/" -- "$1"
