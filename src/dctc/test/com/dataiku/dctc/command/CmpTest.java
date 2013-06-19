6caddf62f7244ef67e54899409255f39b7cd8abd
David Moreira <david.moreira@dataiku.com>
2013-06-19 18:14:52 +0200

    Test for Cmp.
    * src/dctc/test/com/dataiku/dctc/command/CmpTest.java: Here.

---
+++ src/dctc/test/com/dataiku/dctc/command/CmpTest.java
@@ -0,0 +1,41 @@
+package com.dataiku.dctc.command;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import com.dataiku.dctc.Settings;
+import com.dataiku.dctc.configuration.GlobalConf;
+import com.dataiku.dctc.configuration.StructuredConf;
+
+public class CmpTest {
+    private void checkErr(String str) {
+        assertTrue(str.equals(Settings.getErr()));
+    }
+    private void checkOut(String str) {
+        assertTrue(str.equals(Settings.getOut()));
+    }
+
+    @org.junit.Test
+    public void cmp() throws IOException {
+        Settings.setOutputs();
+
+        Cmp cmp = new Cmp();
+        String[] args = new String[] { "/tmp/does not exits", "same as the first argument" };
+        StructuredConf conf = new StructuredConf();
+        conf.parse(GlobalConf.confPath());
+        conf.parseSsh(GlobalConf.sshConfigFile());
+        cmp.setFileBuilder(conf.getFileBuilder());
+        cmp.perform(args);
+        assertTrue(cmp.getExitCode() == 2);
+        checkOut("");
+
+        Settings.setOutputs();
+        args[0] = "/etc/bash.bashrc";
+        args[1] = "/etc/bash.bashrc";
+        cmp.perform(args);
+        assertTrue(cmp.getExitCode() == 0);
+        checkOut("");
+        checkErr("");
+    }
+}
