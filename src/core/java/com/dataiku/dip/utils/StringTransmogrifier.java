317bb2a282c333aceb6be0352ac1f25160f9d0df
Clement Stenac <clement.stenac@dataiku.com>
2013-06-19 11:33:59 +0200

    Add a String transmogrifier

---
+++ src/core/java/com/dataiku/dip/utils/StringTransmogrifier.java
@@ -0,0 +1,35 @@
+package com.dataiku.dip.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Transforms strings into whatever they would like to be.
+ */
+public class StringTransmogrifier {
+    private Set<String> assigned = new HashSet<String>();
+    private String delimiter = "_";
+    
+    public StringTransmogrifier() {
+    }
+    public StringTransmogrifier(String delimiter) {
+        this.delimiter = delimiter;
+    }
+    
+    public void addAlreadyTransmogrified(String in) {
+        if (assigned.contains(in)) {
+            throw new IllegalArgumentException("Input string " + in + " is already in the transmogrifier");
+        }
+        assigned.add(in);
+    }
+    
+    public String transmogrify(String input) {
+        String cur = input;
+        int i = 0;
+        while (assigned.contains(cur)) {
+            cur = input + delimiter + (++i);
+        }
+        assigned.add(cur);
+        return cur;
+    }
+}
