0521ec4dc7b57f24f0c69bba6e261a656dfe8f08
David Moreira <david.moreira@dataiku.com>
2013-06-24 09:07:19 +0200

    clean: Imports.
    * src/core/java/com/dataiku/dip/utils/StreamReplacementReader.java:
      Remove unused.

--- src/core/java/com/dataiku/dip/utils/StreamReplacementReader.java
+++ src/core/java/com/dataiku/dip/utils/StreamReplacementReader.java
@@ -1,9 +1,7 @@
 package com.dataiku.dip.utils;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 import java.nio.CharBuffer;
 
 public class StreamReplacementReader extends Reader {
