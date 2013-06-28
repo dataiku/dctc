dd6507e8f2bc387c92049f3fbdc4d36302d04765
David Moreira <david.moreira@dataiku.com>
2013-06-28 14:11:47 +0200

    style: Indent.
    * src/core/java/com/dataiku/dip/datalayer/SingleRowProcessor.java: Here

--- src/core/java/com/dataiku/dip/datalayer/SingleRowProcessor.java
+++ src/core/java/com/dataiku/dip/datalayer/SingleRowProcessor.java
@@ -3,29 +3,29 @@ package com.dataiku.dip.datalayer;
 /**
  * A processor that processes the rows one by one.
  *  - It may mark a row as deleted
- *  - It may not store or use previous row references 
+ *  - It may not store or use previous row references
  *  - It is single threaded.
  *  - If it excepts, the row is sent in error
- *  
+ *
  * The column factory must be set before init
  */
 public abstract class SingleRowProcessor {
-	protected ColumnFactory cf;
-	
-	public void setColumnFactory(ColumnFactory cf) {
-		this.cf = cf;
-	}
-	
-	/** Called after the ColumnFactory has been set */
-	public abstract void init() throws Exception;
-	
-	public abstract void processRow(Row row) throws Exception;
+    protected ColumnFactory cf;
 
-	/**
-	 * Called after the last row has been processed.
-	 * Note that you may not do anything with the rows here. You are basically only
-	 * allowed to declare a column as deleted
-	 * @throws Exception 
-	 */
-	public abstract void postProcess() throws Exception;
+    public void setColumnFactory(ColumnFactory cf) {
+        this.cf = cf;
+    }
+
+    /** Called after the ColumnFactory has been set */
+    public abstract void init() throws Exception;
+
+    public abstract void processRow(Row row) throws Exception;
+
+    /**
+     * Called after the last row has been processed.
+     * Note that you may not do anything with the rows here. You are basically only
+     * allowed to declare a column as deleted
+     * @throws Exception
+     */
+    public abstract void postProcess() throws Exception;
 }
