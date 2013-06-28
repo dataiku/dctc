29483b93e381196e46175bbd705ab764c4957ac5
David Moreira <david.moreira@dataiku.com>
2013-06-28 15:22:16 +0200

    Moar getters-setters.
    * src/core/java/com/dataiku/dip/partitioning/PartitioningScheme.java:
      Here.

--- src/core/java/com/dataiku/dip/partitioning/PartitioningScheme.java
+++ src/core/java/com/dataiku/dip/partitioning/PartitioningScheme.java
@@ -6,19 +6,16 @@ import java.util.List;
 import java.util.Map;
 
 public class PartitioningScheme {
-    public String filePathPattern;
-    public boolean ignoreNonMatchingFile = false;
-    private List<String> dimensionNames = new ArrayList<String>();
-    private Map<String, Dimension> dimensions = new HashMap<String, Dimension>();
-    public Bucketing bucketing;
 
     public boolean isPartitioned() {
         return dimensions.size() > 0;
     }
 
-    public void addDimension(Dimension dimension) {
+    public PartitioningScheme addDimension(Dimension dimension) {
         dimensionNames.add(dimension.getName());
         dimensions.put(dimension.getName(), dimension);
+
+        return this;
     }
 
     public boolean isEqual(PartitioningScheme other) {
@@ -34,8 +31,50 @@ public class PartitioningScheme {
     public List<String> getDimensionNames() {
         return dimensionNames;
     }
-
     public Map<String, Dimension> getDimensions() {
         return dimensions;
     }
+    public Dimension getDimensions(String name) {
+        return dimensions.get(name);
+    }
+
+    public String getFilePathPattern() {
+        return filePathPattern;
+    }
+    public void setFilePathPattern(String filePathPattern) {
+        this.filePathPattern = filePathPattern;
+    }
+    public PartitioningScheme withFilePathPattern(String filePathPattern) {
+        this.filePathPattern = filePathPattern;
+        return this;
+    }
+    public boolean getIgnoreNonMatchingFile() {
+        return ignoreNonMatchingFile;
+    }
+    public void setIgnoreNonMatchingFile(boolean ignoreNonMatchingFile) {
+        this.ignoreNonMatchingFile = ignoreNonMatchingFile;
+    }
+    public PartitioningScheme withIgnoreNonMatchingFile(boolean ignoreNonMatchingFile) {
+        this.ignoreNonMatchingFile = ignoreNonMatchingFile;
+        return this;
+    }
+    public Bucketing getBucketing() {
+        return bucketing;
+    }
+    public void setBucketing(Bucketing bucketing) {
+        this.bucketing = bucketing;
+    }
+    public PartitioningScheme withBucketing(Bucketing bucketing) {
+        this.bucketing = bucketing;
+        return this;
+    }
+
+    // Attributes
+    private Bucketing bucketing;
+    private boolean ignoreNonMatchingFile = false;
+    private String filePathPattern;
+    private List<String> dimensionNames = new ArrayList<String>();
+    private Map<String, Dimension> dimensions = new HashMap<String, Dimension>();
+
+
 }
