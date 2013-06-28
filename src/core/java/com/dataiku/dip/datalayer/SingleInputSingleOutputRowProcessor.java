966b57e15d88c96f6a807eaf3114630a4b159ba0
David Moreira <david.moreira@dataiku.com>
2013-06-28 12:35:10 +0200

    style: Indent.
    * src/core/java/com/dataiku/dip/datalayer/SingleInputSingleOutputRowProcessor.java:
      Here.

--- src/core/java/com/dataiku/dip/datalayer/SingleInputSingleOutputRowProcessor.java
+++ src/core/java/com/dataiku/dip/datalayer/SingleInputSingleOutputRowProcessor.java
@@ -5,32 +5,32 @@ import com.dataiku.dip.ProcessorWithFactories;
 
 /**
  * A simple processor that has an input, an output, and that can create rows.
- * 
+ *
  * The initialization callbacks must be called in this order:
  *   - set output, factories, context (no particular order)
  *   - init
  */
 public abstract class SingleInputSingleOutputRowProcessor implements SingleInputRowProcessor, SingleOutputRowProcessor, ProcessorWithFactories {
-	protected ProcessorOutput out;
-	protected ColumnFactory cf;
-	protected RowFactory rf;
-	protected Context context;
-	
-	@Override
-	public void setProcessorOutput(ProcessorOutput out) {
-		this.out = out;
-	}
+    protected ProcessorOutput out;
+    protected ColumnFactory cf;
+    protected RowFactory rf;
+    protected Context context;
 
-	@Override
-	public void setFactories(ColumnFactory cf, RowFactory rf) {
-		this.cf = cf;
-		this.rf = rf;
-	}
-	
-	public void setContext(Context context) {
-	    this.context = context;
-	}
-	
-	/** Called after output and factories are set */
-	public abstract void init() throws Exception;
+    @Override
+    public void setProcessorOutput(ProcessorOutput out) {
+        this.out = out;
+    }
+
+    @Override
+    public void setFactories(ColumnFactory cf, RowFactory rf) {
+        this.cf = cf;
+        this.rf = rf;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    /** Called after output and factories are set */
+    public abstract void init() throws Exception;
 }
