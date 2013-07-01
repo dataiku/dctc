92b22d3f67c2f23a86c27e78602604aaef75feba
David Moreira <david.moreira@dataiku.com>
2013-07-01 17:42:04 +0200

    date: Extend.
    * src/dctc/java/com/dataiku/dctc/display/Date.java: Fill, not fill,
      the user decide.

--- src/dctc/java/com/dataiku/dctc/display/Date.java
+++ src/dctc/java/com/dataiku/dctc/display/Date.java
@@ -2,17 +2,21 @@ package com.dataiku.dctc.display;
 
 public class Date {
     public static String getReadableDate(int time) {
+        return getReadableDate(time, true);
+    }
+    public static String getReadableDate(int time, boolean fill) {
         String res = "";
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < div.length; ++i) {
             if (time == 0) {
-                if (i < 2) {
+                if (i < 2 && fill) {
                     res = "0" + unit.charAt(i) + res;
                 }
                 break;
-            } else {
+            }
+            else {
                 int t = time % div[i];
                 time /= div[i];
-                res = "" + t + unit.charAt(i) + res;
+                res = (t < 10 ? "0" : "") + t + unit.charAt(i) + res;
             }
         }
         return res;
