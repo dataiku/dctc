42b74408a090d9ee9b93e888c053e89bc89d5b78
David Moreira <david.moreira@dataiku.com>
2013-07-03 10:33:01 +0200

    grep: Color for the printer.
    * src/dctc/java/com/dataiku/dctc/command/ColorGrepPrinter.java: Here.

---
+++ src/dctc/java/com/dataiku/dctc/command/ColorGrepPrinter.java
@@ -0,0 +1,40 @@
+package com.dataiku.dctc.command;
+
+import com.dataiku.dctc.file.GeneralizedFile;
+
+class ColorGrepPrinter implements GrepPrinter {
+    ColorGrepPrinter(GrepMatcher matcher) {
+        this.matcher = matcher;
+    }
+    public void print(String line) {
+        while(!line.isEmpty()) {
+            if (matcher.match(line)) {
+                int begin = matcher.begin(line);
+                int end = matcher.end(line);
+                System.out.print(line.substring(0, begin));
+                System.out.print("\u001B[1;31m" + line.substring(begin, end) + "\u001B[0m");
+                line = line.substring(end);
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println(line);
+    }
+    public void end(GeneralizedFile file) {
+    }
+
+    public GrepMatcher getMatcher() {
+        return matcher;
+    }
+    public void setMatcher(GrepMatcher matcher) {
+        this.matcher = matcher;
+    }
+    public ColorGrepPrinter withMatcher(GrepMatcher matcher) {
+        this.matcher = matcher;
+        return this;
+    }
+
+    // Attributes
+    private GrepMatcher matcher;
+}
