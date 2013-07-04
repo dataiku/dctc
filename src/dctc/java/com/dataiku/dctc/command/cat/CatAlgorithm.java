1e10edafa3e5f3d28da8bdb7c4342dfbeffc014f
David Moreira <david.moreira@dataiku.com>
2013-07-04 13:53:29 +0200

    fixup! cat: Start to refactore the algorithm with a strategy design pattern.

--- src/dctc/java/com/dataiku/dctc/command/cat/CatAlgorithm.java
+++ src/dctc/java/com/dataiku/dctc/command/cat/CatAlgorithm.java
@@ -1,8 +1,6 @@
 package com.dataiku.dctc.command.cat;
 
-import com.dataiku.dctc.file.GeneralizedFile;
-
 public interface CatAlgorithm {
-    public void run(GeneralizedFile file);
+    public void run();
     public int getExitCode();
 }
