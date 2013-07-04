5d395019a33ac37c81e206c766141d182e693308
David Moreira <david.moreira@dataiku.com>
2013-07-04 17:19:49 +0200

    file: Make standard file public.
    * src/dctc/java/com/dataiku/dctc/file/StandardFile.java: Here.

--- src/dctc/java/com/dataiku/dctc/file/StandardFile.java
+++ src/dctc/java/com/dataiku/dctc/file/StandardFile.java
@@ -6,7 +6,7 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-class StandardFile implements GeneralizedFile {
+public class StandardFile implements GeneralizedFile {
     @Override
     public int compareTo(GeneralizedFile arg0) {
         if (arg0 instanceof StandardFile) {
