cdda0925e01f371a500daeb66387ab4d5fdc8fac
David Moreira <david.moreira@dataiku.com>
2013-07-05 11:42:51 +0200

    tail: Use the new implementation.
    * src/dctc/java/com/dataiku/dctc/command/Tail.java: Here.

--- src/dctc/java/com/dataiku/dctc/command/Tail.java
+++ src/dctc/java/com/dataiku/dctc/command/Tail.java
@@ -1,19 +1,18 @@
 package com.dataiku.dctc.command;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 import java.util.List;
 
 import org.apache.commons.cli.Options;
 import org.apache.commons.io.IOUtils;
 
-import com.dataiku.dctc.GlobalConstants;
 import com.dataiku.dctc.command.abs.Command;
+import com.dataiku.dctc.command.cat.AlgorithmType;
+import com.dataiku.dctc.command.cat.CatAlgorithmFactory;
+import com.dataiku.dctc.command.cat.CatRunner;
 import com.dataiku.dctc.file.GeneralizedFile;
 import com.dataiku.dip.utils.IndentedWriter;
-import com.dataiku.dip.utils.StreamUtils;
 
 public class Tail extends Command {
     public String tagline() {
@@ -48,71 +47,14 @@ public class Tail extends Command {
         }
     }
     private void performLine(List<GeneralizedFile> args) {
-        for (GeneralizedFile arg: args) {
-            if (args.size() > 1) {
-                header(arg);
-            }
-            if (arg.canGetLastLines()) {
-                try {
-                    IOUtils.copyLarge(arg.getLastLines(nbLine()), System.out);
-                } catch (IOException e) {
-                    error(arg.givenName(), e.getMessage(), 1);
-                }
-            } else if (arg.canGetPartialFile()) {
-                long size;
-                try {
-                    size = arg.getSize();
-                } catch (IOException e) {
-                    error(arg.givenName(), e.getMessage(), 1);
-                    continue;
-                }
-                String line = "";
+        CatAlgorithmFactory fact = new CatAlgorithmFactory()
+            .withAlgo(AlgorithmType.TAIL)
+            .withNbLine((int) nbLine()); // FIXME: Pass the number of line to int
 
-                while (size >= 0 && line.split("\n").length <= nbLine()) {
+        CatRunner runner = new CatRunner();
 
-                    StringWriter writer = new StringWriter();
-                    try {
-                        size -= GlobalConstants.FIVE_MIO;
-                        IOUtils.copy(arg.getRange(size, GlobalConstants.FIVE_MIO), writer, "UTF-8");
-                    } catch (IOException e) {
-                        error(arg.givenName(), e.getMessage(), 1);
-                    }
-                    line = writer.toString() + line;
-
-                }
-                String[] lines = line.split("\n");
-                for (int i = Math.max(0, lines.length - (int) nbLine()); i < lines.length; ++i) {
-                    System.out.println(lines[i]);
-                }
-            } else {
-                String[] buf = new String[(int) nbLine()];
-                int idx = 0;
-                BufferedReader in = null;
-                try {
-                    in = StreamUtils.readStream(arg.inputStream());
-                    String line = null;
-
-                    while((line = in.readLine()) != null) {
-                        buf[idx] = line;
-                        idx = (idx + 1) % (int) nbLine();
-                    }
-                }
-                catch (IOException e) {
-                    error(arg.givenName(), "error", e, 2);
-                }
-                finally {
-                    if (in == null) {
-                        IOUtils.closeQuietly(in);
-                    }
-                }
-                for (long i = 0; i < nbLine(); ++i) {
-                    int index = (int) (idx + i) % (int) nbLine;
-                    if (buf[index] != null) {
-                        System.out.println(buf[index]);
-                    }
-                }
-            }
-        }
+        runner.perform(args, true, fact);
+        setExitCode(runner.getExitCode());
     }
     private void performByte(List<GeneralizedFile> args) {
         for (GeneralizedFile arg: args) {
@@ -146,7 +88,7 @@ public class Tail extends Command {
         System.out.print(arg.givenName());
         System.out.println(" <===");
     }
-    public long nbLine() {
+    public int nbLine() {
         if (nbLine == -1) {
             if (hasOption("line")) {
                 nbLine = Integer.parseInt(getOptionValue("line"));
@@ -156,11 +98,11 @@ public class Tail extends Command {
         }
         return nbLine;
     }
-    public Tail nbLine(long nbLine) {
+    public Tail nbLine(int nbLine) {
         this.nbLine = nbLine;
         return this;
     }
-    public long nbByte() {
+    public int nbByte() {
         if (nbByte == -1) {
             if (hasOption("byte")) {
                 nbByte = Integer.parseInt(getOptionValue("byte"));
@@ -168,11 +110,11 @@ public class Tail extends Command {
         }
         return nbByte;
     }
-    public Tail nbByte(long nbByte) {
+    public Tail nbByte(int nbByte) {
         this.nbByte = nbByte;
         return this;
     }
 
-    private long nbLine = -1;
-    private long nbByte = -1;
+    private int nbLine = -1;
+    private int nbByte = -1;
 }
