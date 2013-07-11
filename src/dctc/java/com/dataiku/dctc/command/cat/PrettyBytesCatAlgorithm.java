6fada2a995a55bf52e37fb6cb2d81f592330f827
David Moreira <david.moreira@dataiku.com>
2013-07-11 11:31:56 +0200

    cat: Implement a cat algorithm for pretty char.
    * src/dctc/java/com/dataiku/dctc/command/cat/PrettyBytesCatAlgorithm.java:
      Print all char in a readable format.

---
+++ src/dctc/java/com/dataiku/dctc/command/cat/PrettyBytesCatAlgorithm.java
@@ -0,0 +1,42 @@
+package com.dataiku.dctc.command.cat;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.dataiku.dctc.file.GeneralizedFile;
+import static com.dataiku.dctc.utils.CharUtils.showNonPrintable;
+
+class PrettyBytesCatAlgorithm extends AbstractBytesCatAlgorithm {
+    public PrettyBytesCatAlgorithm(GeneralizedFile file, String cmdname) {
+        super(file, cmdname);
+    }
+    protected void copy(InputStream inputStream, GeneralizedFile file) {
+        try {
+            byte[] buf = new byte[1024];
+            int nbRead;
+            while ((nbRead = inputStream.read(buf)) != -1) {
+                for (int j = 0; j < nbRead; ++j) {
+                    showNonPrintable(System.out, buf[j], showTabulation);
+                }
+            }
+        }
+        catch (IOException e) {
+            yell("Unexpected error while reading " + file.givenName(), e, 2);
+        }
+    }
+
+    // Getters - Setters
+    public boolean getShowTabulation() {
+        return showTabulation;
+    }
+    public void setShowTabulation(boolean showTabulation) {
+        this.showTabulation = showTabulation;
+    }
+    public PrettyBytesCatAlgorithm withShowTabulation(boolean showTabulation) {
+        setShowTabulation(showTabulation);
+        return this;
+    }
+
+    // Attributes
+    private boolean showTabulation;
+}
