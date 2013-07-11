d1d104a2c0fe46b42719fc3ad3d2513f09e348d4
David Moreira <david.moreira@dataiku.com>
2013-07-11 16:59:55 +0200

    color: extend.
    * src/dctc/java/com/dataiku/dctc/utils/Color.java: With bold, char.

--- src/dctc/java/com/dataiku/dctc/utils/Color.java
+++ src/dctc/java/com/dataiku/dctc/utils/Color.java
@@ -9,10 +9,13 @@ public class Color {
     public final static String red = "31m";
     public final static String reset = "0m";
     public final static String bold = "1m";
-    private final static String colorChar = "u001B[";
+    private final static String colorChar = "\u001B[";
 
     public static String color(String color, String msg) {
         return colorChar + color + msg + colorChar + reset;
+    }
+    public static String bcolor(String color, String msg) {
+        return colorChar + "0;1m" + colorChar + color + msg + colorChar + reset;
     }
     public static String purple(String msg) {
         return color(purple, msg);
@@ -33,23 +36,45 @@ public class Color {
         return color(grey, msg);
     }
 
+    public static String purple(char msg) {
+        return color(purple, Character.toString(msg));
+    }
+    public static String blue(char msg) {
+        return color(blue, Character.toString(msg));
+    }
+    public static String yellow(char msg) {
+        return color(yellow, Character.toString(msg));
+    }
+    public static String green(char msg) {
+        return color(green, Character.toString(msg));
+    }
+    public static String red(char msg) {
+        return color(red, Character.toString(msg));
+    }
+    public static String grey(char msg) {
+        return color(grey, Character.toString(msg));
+    }
+
     public static String bpurple(String msg) {
-        return color(bold + ";" + purple, msg);
+        return color(purple, msg);
     }
     public static String bblue(String msg) {
-        return color(bold + ";" + blue, msg);
+        return color(blue, msg);
     }
     public static String byellow(String msg) {
-        return color(bold + ";" + yellow, msg);
+        return color(yellow, msg);
     }
     public static String bgreen(String msg) {
-        return color(bold + ";" + green, msg);
+        return color(green, msg);
     }
     public static String bred(String msg) {
-        return color(bold + ";" + red, msg);
+        return color(red, msg);
     }
     public static String bgrey(String msg) {
-        return color(bold + ";" + grey, msg);
+        return color(grey, msg);
+    }
+    public static String bold(String msg) {
+        return color(bold, msg);
     }
 
     public static boolean getColor() {
