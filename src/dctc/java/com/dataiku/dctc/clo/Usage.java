b6b3618b1c502d214d8517f6e1038877054252e5
David Moreira <david.moreira@dataiku.com>
2013-07-11 17:01:12 +0200

    clo: Usage use the printer.
    * src/dctc/java/com/dataiku/dctc/clo/Usage.java: here.

--- src/dctc/java/com/dataiku/dctc/clo/Usage.java
+++ src/dctc/java/com/dataiku/dctc/clo/Usage.java
@@ -6,65 +6,38 @@ import java.util.List;
 import com.dataiku.dctc.configuration.GlobalConf;
 import com.dataiku.dip.utils.IndentedWriter;
 
+import static com.dataiku.dctc.utils.Color.*;
+
 public class Usage {
     public static void print(List<Option> opts) {
-        List<String> prettyOpts = new ArrayList<String>();
-        List<String> descriptions = new ArrayList<String>();
+        Printer printer = new ColorPrinter();
 
-        StringBuilder sb = new StringBuilder();
+        printer.start();
         for (Option opt: opts) {
-            sb.setLength(0);
+            printer.newOption();
+            // Short
             String shortOpts = opt.getShortOption() != null
                 ? opt.getShortOption().getOpts() : "";
             for (int i = 0; i < shortOpts.length(); ++i) {
-                sb.append('-');
-                sb.append(shortOpts.charAt(i));
-                sb.append(", ");
+                printer.add(shortOpts.charAt(i));
             }
+            // Long
             List<String> longOpts = opt.getLongOption() != null
                 ? opt.getLongOption().getOpts() : new ArrayList<String>();
             for (String longOpt: longOpts) {
-                sb.append("--");
-                sb.append(longOpt);
-                sb.append(", ");
+                printer.add(longOpt);
             }
-            sb.setLength(sb.length() - 2); // Delete the latest comma
-                                           // space.
-
-            // Print the argument name if existing.
+            printer.endOptionListing();
+            // Options
             if (opt.hasOption() && opt.getArgName() != null) {
-                sb.append("=");
-                sb.append(opt.getArgName());
+                printer.addParam(opt.getArgName());
             }
-
-            prettyOpts.add(sb.toString());
-            descriptions.add(opt.getDescription());
+            // Description
+            printer.endOption();
+            printer.addDescription(opt.getDescription());
         }
-
-        IndentedWriter indent = new IndentedWriter();
-        indent.setTermSize(Math.min(120, GlobalConf.getColNumber()));
-
-        int maxLength = maxLength(prettyOpts);
-        indent.setIndentSize(maxLength + 6);
-        for (int i = 0; i < prettyOpts.size(); ++i) {
-            String prettyOpt = prettyOpts.get(i);
-            System.out.print("  ");
-            System.out.print(prettyOpt);
-
-            // local line indentation.
-            indent.withStartIndex(prettyOpt.length() + 2)
-                .setFirstLineIndentsize(maxLength - prettyOpt.length() + 2);
-            indent.print(descriptions.get(i));
-        }
+        printer.print();
     }
 
-    // Privates
-    private static int maxLength(List<String> ls) {
-        int res = Integer.MIN_VALUE;
-        for (String l: ls) {
-            res = Math.max(res, l.length());
-        }
-        return res;
-    }
 }
 
