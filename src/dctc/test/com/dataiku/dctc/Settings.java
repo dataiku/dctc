bdabe58ed86bef4a9befb1d946d8affec55a5bb2
David Moreira <david.moreira@dataiku.com>
2013-07-26 15:05:32 +0200

    test: Extend settings with out/err reset function.
    * src/dctc/test/com/dataiku/dctc/Settings.java: add a functin for
      reset the outputs.

--- src/dctc/test/com/dataiku/dctc/Settings.java
+++ src/dctc/test/com/dataiku/dctc/Settings.java
@@ -6,11 +6,17 @@ import java.io.UnsupportedEncodingException;
 
 public class Settings {
     public static void setErr() throws UnsupportedEncodingException {
+        if (stErr == null) {
+            stErr = System.err;
+        }
         err = new ByteArrayOutputStream();
         PrintStream errPrint = new PrintStream(err, true, "UTF-8");
         System.setErr(errPrint);
     }
     public static void setOut() throws UnsupportedEncodingException {
+        if (stErr == null) {
+            stOut = System.out;
+        }
         out = new ByteArrayOutputStream();
         PrintStream outPrint = new PrintStream(out, true, "UTF-8");
         System.setOut(outPrint);
@@ -27,7 +33,14 @@ public class Settings {
         setOut();
     }
 
+    public static void resetOutputs()  {
+        System.setErr(stErr);
+        System.setOut(stOut);
+    }
+
     private static ByteArrayOutputStream err;
     private static ByteArrayOutputStream out;
 
+    private static PrintStream stErr;
+    private static PrintStream stOut;
 }
