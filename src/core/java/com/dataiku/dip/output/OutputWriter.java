a9f953f99d9dfa0154fee1e4512bbd82ab0c9682
Clement Stenac <clement.stenac@dataiku.com>
2013-08-21 13:34:28 +0200

    Fix exceptions

--- src/core/java/com/dataiku/dip/output/OutputWriter.java
+++ src/core/java/com/dataiku/dip/output/OutputWriter.java
@@ -1,12 +1,13 @@
 package com.dataiku.dip.output;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import com.dataiku.dip.datalayer.ColumnFactory;
 import com.dataiku.dip.datalayer.ProcessorOutput;
 
 public abstract class OutputWriter implements ProcessorOutput{
-    public abstract void init(ColumnFactory cf) throws IOException;
+    public abstract void init(ColumnFactory cf) throws Exception;
     /** This should be the real number of written bytes (for example, after GZip) */
     public abstract long writtenBytes() throws IOException;
 }
